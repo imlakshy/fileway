5945468ecb1e1c42e993a246f13e45434a7477ad
Lakshya Kashyap <lakshya@Lakshyas-MacBook-Air.local>
2025-12-17 03:16:29 +0530

    ...

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDNEtd7dYpl3Vf6lwiClZqd7WMx8zl2in4",
-  authDomain: "fileway-0001.firebaseapp.com",
-  projectId: "fileway-0001",
-  storageBucket: "fileway-0001.firebasestorage.app",
-  messagingSenderId: "29441474832",
-  appId: "1:29441474832:web:ea41f57b9fb77fa13ea595"
+ 
 };
 
 // Initialize Firebase
